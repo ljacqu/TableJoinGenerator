0520f07ebd718eea367f278c92a10d2fd592f9ae
ljacqu <ljacqu@users.noreply.github.com>
2024-11-09 18:56:02 +0100

    TS: Use TableDefinitions instead of __table global

--- project/TableDefinitions.ts
+++ project/TableDefinitions.ts
@@ -2,18 +2,51 @@ namespace QB {
 
     export class TableDefinitions {
 
-        private static tables?: any;
+        private static tables: TableDefs;
 
         private constructor() {
         }
 
         static init(__tables: any) {
-            this.tables = __tables;
+            this.tables = __tables as TableDefs;
         }
 
         static getColumnType(table: string, column: string): string {
-            return this.tables[table].columns[column];
+            return this.tables![table].columns[column];
+        }
+
+        static getColumns(table: string): ColumnDef {
+            return this.tables[table].columns;
+        }
+
+        static getReferences(table: string): TableReference {
+            return this.tables[table].references;
+        }
+
+        static getAllTables(): TableDefs {
+            return this.tables;
         }
     }
 
+    type TableDefs = {
+        [tableName: string]: TableDef
+    };
+
+    export type TableDef = {
+        alias?: string;
+        columns: ColumnDef;
+        references: TableReference;
+        highlights: { };
+    };
+
+    export type TableReference = {
+        [columnName: string]: {
+            table: string;
+            column: string;
+        }
+    };
+
+    export type ColumnDef = {
+        [columnName: string]: string
+    };
 }
